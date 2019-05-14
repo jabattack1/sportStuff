3053ce8cd2521566909e01e645b515dd535b28a0
jabattack1 <peter.hahn@live.com>
2019-05-14 17:37:12 -0400

    changeTitle

---
+++ webpack.config.dev.js
@@ -0,0 +1,43 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'source-map',
+  entry: [
+    'webpack-hot-middleware/client',
+    './client/cards'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  module: {
+    loaders: [
+    // js
+    {
+      test: /\.js$/,
+      loaders: ['babel'],
+      include: path.join(__dirname, 'client')
+    },
+    // CSS
+    { 
+      test: /\.styl$/, 
+      include: path.join(__dirname, 'client'),
+      loader: 'style-loader!css-loader!stylus-loader'
+    },
+    {
+      test: /src(\/|\\).*\.css$/,
+      loader: 'style!css'
+    },
+    {
+      test: /src(\/|\\).*\.scss$/,
+      loader: 'style!css!sass'
+    }
+    ]
+  }
+};
